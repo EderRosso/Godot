b51a81cfcb3f2404c71cedc9f61e7ad1ea6e03e9
Adrianiani <monteiroadriani@gmail.com>
2025-08-02 13:10:09 -0300

    aula1 Godot-poing

---
+++ pong/collision_shape_2d.gd
@@ -0,0 +1,12 @@
+extends CollisionShape2D
+
+var velocidade_bola : int = 500
+var poicao_inicial : Vector2 = Vector2(640, 320)
+
+func _ready() -> void:
+	pass 
+
+
+
+func _process(delta: float) -> void:
+	pass
